fd9ea4dc7de52298cd5a4cb25aed32ed4951d760
TinaHeiligers <tina.heiligers@gmail.com>
2018-08-18 11:38:29 -0700

    Adds documentation to the readme

--- README.md
+++ README.md
@@ -69,17 +69,54 @@ GET request to https://u1l461m5nl.execute-api.us-east-1.amazonaws.com/dev/hello-
 
 If the current handler.js file is small, add new functions directly into the handler file.
 Use the same config for each, starting with `module.exports.<functionName> = (event, context, callback) {...}`
-
+Note: if you use an external package, remember to install it with npm. The serverless quick-start docs do not mention that.
 
 ## Provisioning infrastructure (e.g. a database)
 
 One can provision a DynamoDB table (noSQL) using [CloudFormation](https://aws.amazon.com/cloudformation/)
 Add configuration to the serverless.yml file, using the [CloudFormation syntax](http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-dynamodb-table.html)
 
-
 ## Deploy
 
 run `sls deploy` to deploy the functions.
+
+## Metrics
+
+The `sls metrics` command will return service level information about the number of times the functions have been called and some other metrics. For example:
+```
+Service wide metrics
+August 17, 2018 11:22 AM - August 18, 2018 11:22 AM
+
+Invocations: 4
+Throttles: 0
+Errors: 2
+Duration (avg.): 73.59ms
+```
+
+To get function level metrics, add the `-f` flag, followed by the function name. For example:
+`sls metrics -f saveName` returns:
+```
+saveName
+August 17, 2018 11:27 AM - August 18, 2018 11:27 AM
+
+Invocations: 3
+Throttles: 0
+Errors: 2
+Duration (avg.): 40.45ms
+```
+
+(the erros above were caused by forgetting to install the aws-sdk).
+
+The command `sls logs -f <function Name>` will provide function logs.
+For example:
+`sls logs -f saveName` returns:
+
+```
+START RequestId: 3368b1ed-a315-11e8-b5ab-f564e5b6dd7e Version: $LATEST
+2018-08-18 11:33:29.381 (-07:00)  3368b1ed-a315-11e8-b5ab-f564e5b6dd7e  HELLO! Request to save name Alex with age 24
+END RequestId: 3368b1ed-a315-11e8-b5ab-f564e5b6dd7e
+REPORT RequestId: 3368b1ed-a315-11e8-b5ab-f564e5b6dd7e  Duration: 110.02 ms Billed Duration: 200 ms   Memory Size: 1024 MB  Max Memory Used: 34 MB
+```
 
 ## Current Functions
 
@@ -96,3 +133,7 @@ endpoints:
 functions:
   helloWorld: hello-world-dev-helloWorld
   helloName: hello-world-dev-helloName
+
+## Removing a service (to prevent incurring charges)
+
+Issue the comman `sls remove`
--- handler.js
+++ handler.js
@@ -1,7 +1,8 @@
-'use strict';
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+
 const NAMES_TABLE = process.env.NAMES_TABLE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 
 module.exports.saveName = (event, context, callback) => {
   let response = {
@@ -12,7 +13,7 @@ module.exports.saveName = (event, context, callback) => {
   };
   const name = event.queryStringParameters.name;
   const age = event.queryStringParameters.age;
-  console.log(`Request to save name ${name} with age ${age}`);
+  console.log(`HELLO! Request to save name ${name} with age ${age}`);
 
   const params = {
     TableName: NAMES_TABLE,
@@ -32,19 +33,19 @@ module.exports.saveName = (event, context, callback) => {
     }
     response.body = JSON.stringify({ name, age })
     callback(null, response);
-  })
+  });
 }
 
 module.exports.getName = (event, context, callback) => {
   let response = {
-    statusCode = 200;
+    statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
     }
   };
 
   const name = event.queryStringParameters.name;
-  console.log(`Request tp retrieve name ${name}`);
+  console.log(`Request to retrieve name ${name}`);
 
   const params = {
     TableName: NAMES_TABLE,
@@ -62,7 +63,7 @@ module.exports.getName = (event, context, callback) => {
       callback(null, response);
     }
     if (result.Item) {
-      const { name, age } = result.Item;
+      const {name, age } = result.Item;
       response.body = JSON.stringify({ name, age })
 
       callback(null, response);
@@ -74,35 +75,3 @@ module.exports.getName = (event, context, callback) => {
     }
   });
 }
-
-module.exports.helloWorld = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-};
-
-module.exports.helloName = (event, context, callback) => {
-  let name = 'stranger';
-  if (event.queryStringParameters && event.queryStringParameters.name) {
-    name = event.queryStringParameters.name;
-  }
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', //Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: `Hello, ${name}!`
-    })
-  };
-  callback(null, response);
-};
